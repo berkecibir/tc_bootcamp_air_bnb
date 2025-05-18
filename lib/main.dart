f8185074d2e50a65c337cafa645ba84ba38be15b
Berke Atahan <berkecibir@gmail.com>
2025-05-18 13:36:29 +0300

    Changed project initial route to Splash Page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:tc_bootcamp_air_bnb/feat/core/configs/theme/app_theme.dart';
 import 'package:tc_bootcamp_air_bnb/feat/core/init/app_init.dart';
 import 'package:tc_bootcamp_air_bnb/feat/core/router/app_router.dart';
 import 'package:tc_bootcamp_air_bnb/feat/core/widgets/navigation_helper/navigation_helper.dart';
-import 'package:tc_bootcamp_air_bnb/feat/presentation/main/main_page.dart';
 import 'package:tc_bootcamp_air_bnb/feat/presentation/splash/splash_page.dart';
 import 'package:tc_bootcamp_air_bnb/providers/bloc_provider_set_up.dart';
 
@@ -25,7 +24,7 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Airbnb',
         navigatorKey: Navigation.navigationKey,
-        initialRoute: MainPage.id,
+        initialRoute: SplashPage.id,
         routes: AppRoutes.routes,
       ),
     );
